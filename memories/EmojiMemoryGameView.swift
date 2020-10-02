636fa76f6cbc2abd9e698c8185e999b4c96cad2a
zhuxinyu <81219723@qq.com>
2020-10-02 15:24:44 +0800

    implement reactive by ObservedObject&ObservableObject

--- memories/EmojiMemoryGameView.swift
+++ memories/EmojiMemoryGameView.swift
@@ -1,5 +1,5 @@
 //
-//  ContentView.swift
+//  EmojiMemoryGameView.swift
 //  memories
 //
 //  Created by liwei43 on 2020/9/29.
@@ -8,14 +8,16 @@
 
 import SwiftUI
 
-struct ContentView: View {
-    var viewModel: EmojiMemoryGame
+struct EmojiMemoryGameView: View {
+    @ObservedObject var viewModel: EmojiMemoryGame
     
     var body: some View {
         HStack {
             ForEach(viewModel.cards) { card in
                 CardView(card: card).onTapGesture {
                     self.viewModel.choose(card: card)
+//                    print(Unmanaged<AnyObject>.passUnretained(self.viewModel as AnyObject).toOpaque())
+
                 }
             }
         }
@@ -44,6 +46,6 @@ struct CardView: View {
 
 struct ContentView_Previews: PreviewProvider {
     static var previews: some View {
-        ContentView(viewModel: EmojiMemoryGame())
+        EmojiMemoryGameView(viewModel: EmojiMemoryGame())
     }
 }
--- memories/MemoryGame.swift
+++ memories/MemoryGame.swift
@@ -11,12 +11,25 @@ import Foundation
 struct MemoryGame<CardContent> {
     var cards: Array<Card>
     
-    func choose(card: Card) {
+    mutating func choose(card: Card) {
         print("card choosen: \(card)")
+        let index:Int = self.index(of: card)
+        self.cards[index].isFaceUP = !self.cards[index].isFaceUP
+    }
+    
+    func index(of card:Card) -> Int {
+        for index in 0..<cards.count {
+            if self.cards[index].id == card.id {
+                return index
+            }
+        }
+        return 0
     }
     
     init(numberOfPairsOfCards: Int, cardContentFactory: (Int) -> CardContent) {
         cards = Array<Card>()
+//        print(Unmanaged<AnyObject>.passUnretained(cards as AnyObject).toOpaque())
+
         for pairIndex in 0..<numberOfPairsOfCards {
             let content = cardContentFactory(pairIndex)
             cards.append(Card(content: content, id: pairIndex * 2))
